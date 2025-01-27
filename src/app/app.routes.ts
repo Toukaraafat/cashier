4f676302583a06474e9b32c2c82b0cb34f143829
Toukaraafat <tokaraafat25@gmail.com>
2025-01-27 17:16:05 +0200

    commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,23 +1,16 @@
 import { Routes } from '@angular/router';
-<<<<<<< HEAD
-import { LoginComponent } from './login/login.component'; // Adjust the path if needed
-
-export const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' }, // Default route
-  { path: 'login', component: LoginComponent },         // Login route
-  { path: '**', redirectTo: 'login' }                  // Wildcard route for invalid paths
-=======
+import { LoginComponent } from './login/login.component'; 
 import { OrdersComponent } from './orders/orders.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-    {path: 'orders', component:OrdersComponent},
-    {path: '', component: HomeComponent}, 
-    {path: '', component: SidebarComponent}, 
-    {path: '', component: NavbarComponent},
-
-
->>>>>>> b2a039b134347e1c2856be570bf7a3070fe0cf29
-];
+    { path: '', redirectTo: 'login', pathMatch: 'full' }, 
+    { path: 'login', component: LoginComponent },        
+    { path: '**', redirectTo: 'login' },          
+    { path: 'orders', component: OrdersComponent },
+    { path: '', component: HomeComponent },
+    { path: '', component: SidebarComponent },
+    { path: '', component: NavbarComponent },
+]
