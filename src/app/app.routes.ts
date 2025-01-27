2ff96ccc9bb9cfd14e638122cfb308fa94d2d9af
Toukaraafat <tokaraafat25@gmail.com>
2025-01-27 17:03:50 +0200

    commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { OrdersComponent } from './orders/orders.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'orders', component:OrdersComponent},
+    {path: '', component: HomeComponent}, 
+    {path: '', component: SidebarComponent}, 
+    {path: '', component: NavbarComponent},
+
+
+];
