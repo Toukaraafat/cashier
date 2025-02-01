2d958500a6a586a21a963e792c53fa09369d5257
Toukaraafat <tokaraafat25@gmail.com>
2025-02-01 22:19:32 +0200

    commit

--- src/app/categories/categories.component.ts
+++ src/app/categories/categories.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input, output, OnInit, } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { ProductCardComponent } from "../product-card/product-card.component";
+import { ProductsService } from '../services/products.service';
 
 @Component({
   selector: 'app-categories',
-  imports: [RouterLink ,RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, CommonModule, ProductCardComponent],
   templateUrl: './categories.component.html',
   styleUrl: './categories.component.css'
 })
-export class CategoriesComponent {
+export class CategoriesComponent implements OnInit{
+  constructor(private productsRequestService: ProductsService) {}
+  baseurl: any;
+  products :any ;
+  @Input() product: any;
 
+  // ngOnInit(): void {
+  //   this.baseurl = this.productsRequestService.getProducts().subscribe((response : any) => this.products = response);
+  // }
+  ngOnInit() {
+    this.productsRequestService.getProducts().subscribe({
+      next: (data: any) => {
+        console.log('Products:', data);
+        this.products = data.products;
+      },
+      error: (err) => console.error('Error fetching products:', err)
+    });
+  }
+  recieveFromProduct(id: any) {
+    console.log("recieve")
+    this.products = this.products.filter((product: { id: any; }) => product.id !== id)
+  }
 }
---
+++ src/app/product-card/product-card.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-product-card',
+  imports: [],
+  templateUrl: './product-card.component.html',
+  styleUrl: './product-card.component.css'
+})
+export class ProductCardComponent {
+  @Input() product: any;
+  @Output() sendToCategories = new EventEmitter(); // bywsl value mn component l component
+
+  deleteProduct(id: string) {
+    console.log(id)
+    this.sendToCategories.emit(id) //method byakhod l value de todyha ll parent
+  }
+}
