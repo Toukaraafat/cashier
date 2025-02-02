58f6be030ac53d17f265cf7ea2002bd0dde065f4
Toqa-Hatem <toqahatem802@gmail.com>
2025-02-02 13:09:32 +0200

    a

---
+++ src/app/pill-details/pill-details.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pill-details',
+  imports: [],
+  templateUrl: './pill-details.component.html',
+  styleUrl: './pill-details.component.css'
+})
+export class PillDetailsComponent {
+
+}
